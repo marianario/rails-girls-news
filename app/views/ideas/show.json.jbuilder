07a88cc53f1dfbad0daf85d3f99b79ffc56b1944
Mariana Rio <mariana.s.rio@gmail.com>
2015-12-12 14:50:40 -0200

    Estrutura inicial do projeto

---
+++ app/views/ideas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @idea, :id, :name, :string, :description, :picture, :string, :created_at, :updated_at
---
+++ db/migrate/20151212125213_create_ideas.rb
@@ -0,0 +1,13 @@
+class CreateIdeas < ActiveRecord::Migration
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.string :string
+      t.text :description
+      t.string :picture
+      t.string :string
+
+      t.timestamps null: false
+    end
+  end
+end
